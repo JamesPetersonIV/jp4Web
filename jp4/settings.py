d9c1ecdb68d638e2bda1924afb073956958fdb8b
JamesPetersonIV <61022550+JamesPetersonIV@users.noreply.github.com>
2024-11-04 19:08:57 -0500

    update static files

--- jp4/settings.py
+++ jp4/settings.py
@@ -64,6 +64,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # Add the account middleware:
     'allauth.account.middleware.AccountMiddleware',
 ]
 
